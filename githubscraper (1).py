afd6699a9b3434f9c6d136bd1c7c66c73d044e99
Tevon Strand-Brown <tevonstrand-brown@Tevon.S-MBPro>
2016-08-22 11:30:47 -0700

    Updated from work comp

---
+++ githubscraper (1).py
@@ -0,0 +1,17 @@
+import requests
+import urllib3
+
+main()
+
+def main():
+	setup()
+	gather():
+	parse():
+
+
+def parse():
+
+
+def setup():
+
+
--- githubscraper.py
+++ githubscraper.py
@@ -1,17 +1,38 @@
-import requests
+import stdio
 import urllib3
+import requests
 
-main()
+
 
 def main():
-	setup()
-	gather():
-	parse():
+	run_script()
+	parse_data()
+	write_file()
+	return
+
+def run_script():
+	'''
+	location = raw_input('Enter a location: ')
+	num_repos = '>5'
+	num_repos = raw_input('Number of repos, range say >20')
+	num_followers = '>5'
+	num_followers = raw_input('Number of followers: same range as above! ')
+	'''
+	##search_string = 'https://api.github.com/search/users?q='+'location:"'+location+'"'+'+repos:'+num_repos
+	search_string = 'https://api.github.com/tevonsb/emails'
+	r = requests.get(search_string, auth = ('tevonsb', 'T3v0ni23'))
+	stdio.writeln(r.status_code)
+	stdio.writeln(r.headers)
+
+	##stdio.writeln(r.json())
+	##stdio.writeln('Your search returned '+r.total_count+' results.')
+	return
 
 
-def parse():
+def parse_data():
+	return
 
+def write_file():
+	return
 
-def setup():
-
-
+main()
