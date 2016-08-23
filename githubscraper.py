e04d59e17ce8355fbbb844b052fb4cf528cf77dc
Tevon Strand-Brown <tevonstrand-brown@Tevon.S-MBPro>
2016-08-23 16:47:17 -0700

    Divided into three separate methods

--- githubscraper.py
+++ githubscraper.py
@@ -3,35 +3,75 @@ import urllib3
 import requests
 import github
 
+NUM_FOLLOWERS = 20
+NUM_REPOS = 10
+LOCATION = 'San Francisco, CA'
 
+
+from github import Github
 def main():
 	run_script()
-	parse_data()
 	write_file()
 	return
 
 def run_script():
-	'''
-	gh = GitHub('tevonsb', 'T')
-	location = raw_input('Enter a location: ')
-	num_repos = '>5'
-	num_repos = raw_input('Number of repos, range say >20')
-	num_followers = '>5'
-	num_followers = raw_input('Number of followers: same range as above! ')
-	'''
-	##search_string = 'https://api.github.com/search/users?q='+'location:"'+location+'"'+'+repos:'+num_repos
-	search_string = 'https://api.github.com/tevonsb/emails'
-	r = requests.get('https://api.github.com/?access_token= 39c1ad610c6c01fb0f2c30a68d2c24e54aa6ed3e')  
-	stdio.writeln(r.status_code)
-	stdio.writeln(r.headers)
 
-	##stdio.writeln(r.json())
-	##stdio.writeln('Your search returned '+r.total_count+' results.')
+
+
+	gh = Github('39c1ad610c6c01fb0f2c30a68d2c24e54aa6ed3e')
+
+	whats_wanted = raw_input("type user (To set a base user), Search (To enter an advanced search string) or keyword (for a keyword based search): ")
+	if whats_wanted == 'user': collect_user(gh)
+	elif whats_wanted == 'search': collect_searched(gh)
+	elif whats_wanted == 'keyword': collect_keyword(gh, 'hello')
+
 	return
 
 
-def parse_data():
+def collect_keyword(gh, search_term):
+
+
+	users = gh.search_users(search_term)
+
+	qual_users = set()
+	count = 0
+	for user in users:
+		if user==None: continue
+		## Checking Needed profile criteria
+		if user.location == None or user.location not in LOCATION or user.name == None: continue
+		print("Trying with:"+ user.name)
+		check_user(user, qual_users)
+		count+=1
+		if(count > 30): break
+
+
+	result = ""
+	for user in qual_users:
+		result += user.name + ' ' + str(user.followers) + user.location + '\n'
+
+	print(result)
 	return
+
+def collect_user(gh, user):
+	user_name = 'tevonsb'
+	base_user = gh.get_user(user_name)
+	for
+	return
+
+def collect_searched(gh):
+	return
+
+
+
+
+
+def check_user(user, qual_users):
+	if(user.followers >= NUM_FOLLOWERS and user.public_repos >= NUM_REPOS and (user.location in LOCATION)):
+		qual_users.add(user)
+	return
+
+
+
 
 def write_file():
 	return
