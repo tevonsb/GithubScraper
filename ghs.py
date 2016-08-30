a3e9361c81fae16bf224026c7d7b852cd59c7375
Tevon Strand-Brown <tevonstrand-brown@Tevon.S-MBPro>
2016-08-30 16:57:20 -0700

    Got most working

--- ghs.py
+++ ghs.py
@@ -1,11 +1,11 @@
 import github
 import csv
 import sys
+from time import gmtime, strftime
 
 NUM_FOLLOWERS = 20
 NUM_REPOS = 10
 LOCATION = 'San Francisco, CA'
-LIMIT = 10
 LIMIT_FOLLOWERS = 10
 q_size_limit = 3
 qual_users = set()
@@ -14,38 +14,48 @@ used_emails = set()
 
 from github import Github
 def main():
-	setup()
-	run_script()
+	inputs = ['', '']
+	setup(inputs)
+	run_script(inputs)
 	print_list()
-	write_file()
+	write_file(inputs)
 	return
 
-def setup():
+def setup(inputs):
 	with open('prev_found.txt', 'r') as prev_file:
-		curr_email = prev_file.readline()
+		curr_email = prev_file.readline().strip()
 		while not curr_email == '':
 			used_emails.add(curr_email)
-			curr_email = prev_file.readline()
+			print(curr_email)
+			curr_email = prev_file.readline().strip()
+	global LIMIT
+	get_input(inputs)
 	return
 
-def get_input(whats_wanted_ptr, input_word_ptr):
+def get_input(inputs):
 	with open("input_file.txt", 'r') as input_file:
 		read_first_line = input_file.readline()
-		*whats_wanted_ptr = read_first_line.substr(read_first_line.get_index_of(':')) ##Sudo code, figure out how to actually code this.
-		
+		whats_wanted = read_first_line[read_first_line.index(':')+1:] ##Sudo code, figure out how to actually code this.
+		read_second = input_file.readline()
+		input_word = read_second[read_second.index(':')+1:]
+		inputs[0] = whats_wanted
+		inputs[1] = input_word
+		limit_line = input_file.readline()
+		LIMIT = limit_line[limit_line.index(':')+1].strip()
+		print(LIMIT)
 
 
-def run_script():
+def run_script(inputs):
 
 
 	gh = Github('39c1ad610c6c01fb0f2c30a68d2c24e54aa6ed3e')
 
-	whats_wanted = raw_input("type user (To set a base user), Search (To enter an advanced search string) or keyword (for a keyword based search): ")
-	input_word = ''
-	get_input(&whats_wanted, &input_word)
-	if whats_wanted == 'user': collect_user(gh, input_word)
-	elif whats_wanted == 'search': collect_searched(gh)
-	elif whats_wanted == 'keyword': collect_keyword(gh, input_word)
+	##whats_wanted = raw_input("type user (To set a base user), Search (To enter an advanced search string) or keyword (for a keyword based search): ")
+	inputs[0] = inputs[0].lower().strip()
+	inputs[1] = inputs[1].lower().strip()
+	if inputs[0] in 'user' or 'user' in inputs[0]: collect_user(gh, inputs[1])
+	elif inputs[0] == 'search': collect_searched(gh)
+	elif inputs[0] == 'keyword': collect_keyword(gh, inputs[1])
 
 	return
 
@@ -66,13 +76,12 @@ def collect_keyword(gh, search_term):
 		if(count > LIMIT): break
 	return
 
-def collect_user(gh):
-	user_name = 'mojombo'
-	base_user = gh.get_user(user_name)
+def collect_user(gh, user_name):
+	base_user = gh.get_user('mojombo')
 	user_queue = []
 	user_queue.append(base_user)
 	count = 0;
-	while user_queue and count < LIMIT:
+	while user_queue:
 		curr_user = user_queue.pop(0)
 		count += 1
 		if not len(user_queue)>LIMIT:
@@ -80,10 +89,12 @@ def collect_user(gh):
 			sys.stdout.write('\nEvaluating user who '+ curr_user.name+ ' follows')
 			for user in curr_user.get_followers():
 				sys.stdout.write('. ')
+				sys.stdout.flush()
 				if count_2 > LIMIT_FOLLOWERS: break
 				if protect_user(user) and check_user(user):
 					user_queue.append(user)
 					qual_users.add(user)
+					if len(qual_users) >= LIMIT: return
 					qual_emails.add(user.email)
 					print("\nadding "+ user.name)
 					count_2 += 1
@@ -95,7 +106,9 @@ def collect_searched(gh):
 
 def protect_user(user):
 	if user==None or user.location == None or user.email == None or user.name == None or user.followers == None or user.public_repos == None: return False
-	if user.email in qual_emails or user.email in used_emails: 
+	if user.email in qual_emails: return False 
+	if user.email in used_emails: 
+		print("found repeat email")
 		return False
 	return True
 
@@ -107,18 +120,19 @@ def check_user(user):
 def print_list():
 	result = ""
 	for user in qual_users:
-		result += user.name + ' ' + str(user.followers) + user.location + '\n'
+		result += user.name + ' ' + str(user.followers).encode('utf-8') + user.location + '\n'
 
 	print(result)
 	return
 
 
-def write_file():
+def write_file(inputs):
 	print('Writing to CSV format.')
-	with open('results.csv', 'w+') as cand_file:
+	file_name = inputs[0]+'_'+inputs[1]+strftime('%m-%d %H_%M_%S')+'.csv'
+	with open(file_name, 'w+') as cand_file:
 		writer = csv.writer(cand_file)
 		for user in qual_users:
-			user_list = [user.name, user.email, user.location, user.followers, user.url]
+			user_list = [user.name.encode('utf-8'), user.email, user.location, user.followers, 'github.com/'+user.login]
 			writer.writerow(user_list)
 	with open('prev_found.txt', 'a') as email_file:
 		for email in qual_emails:
